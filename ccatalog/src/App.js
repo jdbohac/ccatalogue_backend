f47be573eb0ca42b2beadd2edfa34e02bc5b57f0
jdbohac <jeffrey.bohac@gmail.com>
2023-04-21 09:26:09 -0400

    setting up capstone project frontend

---
+++ ccatalog/src/App.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import './App.css';
+
+function App() {
+const [tools, setTools] = useState([])
+
+
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default App;
