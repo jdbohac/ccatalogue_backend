0cc550ded18dfc3fafe10013dbb96ef29508866e
jdbohac <jeffrey.bohac@gmail.com>
2023-04-24 15:00:00 -0400

    rearranging file structure for deployment

---
+++ src/components/tools/ListTools.js
@@ -0,0 +1,31 @@
+import { Box, Button, Grid, Paper, Typography } from '@mui/material'
+import React from 'react'
+
+const ListTools = (props) => {
+
+  return (
+    <>
+    {props.tools.length ? 
+    <>
+    {props.tools.map(tool => {
+      return(
+      <Grid item lg={4} xs={10} mt={2} key={tool.id} onClick={() => props.showTool(tool.id)}>
+      <Paper>
+      <Box p={3}>
+        <Typography variant='h3'>{tool.name}</Typography>
+        <Typography color="secondary.light" variant='h5'>Brand: {tool.brand}</Typography>
+        <Box textAlign="right">
+            <p>Qty: {tool.qty}</p>
+            </Box>
+            </Box>
+          </Paper >
+      </Grid>
+      )
+      })}
+      </>
+      : null}
+    </>
+  )
+}
+
+export default ListTools
